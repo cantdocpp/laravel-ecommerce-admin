a5f9d497b235b229e61610d4733c6b02dad7a1fe
cantdocpp <cihuyman95@gmail.com>
2020-06-08 18:40:04 +0700

    feat: now user we can add new user from admin dashboard

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class UserController extends Controller
 {
@@ -16,6 +19,39 @@ class UserController extends Controller
     }
 
     public function store(Request $request) {
-        dd($request->all());
+        $validateData = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|numeric',
+            'password' => 'required|min:6',
+            'confirm' => 'same:password|min:6',
+            'role' => 'required'
+        ]);
+
+        if ($validateData->fails()) {
+            return redirect()
+                    ->back()
+                    ->withErrors($validateData)
+                    ->withInput();
+        }
+
+        /*
+            $request->all() = 
+            [
+                "_token" => "w20TUSDav82Ke4Kem7arbzQmwj7B7TM9pv3sz1x6"
+                "name" => null
+                "email" => null
+                "phone" => null
+                "password" => null
+                "confirm" => null
+                "role" => null
+            ]
+        */
+        $userData = $request->only('name', 'email', 'phone', 'role');
+        $userData['password'] = Hash::make($request->password);
+        $user = User::create($userData);
+        $user->save();
+
+        return redirect(route('admin.users'));
     }
 }
--- resources/views/admin/users/userCreate.blade.php
+++ resources/views/admin/users/userCreate.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="user-create">
+    <a href="{{ route('admin.users') }}" class="user-create-back">
+            Get Back
+        </a>
         <h1 class="user-create-header">
             New user registration
         </h1>
@@ -10,33 +13,86 @@
             <div class="user-create__row">
                 <label class="user-create__label">
                     <div class="user-create__desc">Full name</div>
-                    <input type="text" name="name" class="user-create__input" placeholder="Ex: William Gunawan">
+                    <input 
+                        type="text" 
+                        name="name" 
+                        class="user-create__input @error('name') user-create__input--error @enderror" 
+                        placeholder="Ex: William Gunawan"
+                        value="{{ old('name') }}"
+                    />
+                    @error('name')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
                 <label class="user-create__label">
                     <div class="user-create__desc">Email</div>
-                    <input type="text" name="email" class="user-create__input" placeholder="Ex: you@example.com">
+                    <input 
+                        type="text" 
+                        name="email" 
+                        class="user-create__input @error('email') user-create__input--error @enderror" 
+                        placeholder="Ex: you@example.com"
+                        value="{{ old('email') }}"
+                    />
+                    @error('email')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
                 <label class="user-create__label">
                     <div class="user-create__desc">Phone number</div>
-                    <input type="number" name="phone" class="user-create__input" placeholder="Ex: 08**********">
+                    <input 
+                        type="number" 
+                        name="phone" 
+                        class="user-create__input @error('phone') user-create__input--error @enderror" 
+                        placeholder="Ex: 08**********"
+                        value="{{ old('phone') }}"
+                    />
+                    @error('phone')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
                 <label class="user-create__label">
                     <div class="user-create__desc">Password</div>
-                    <input type="password" name="password" class="user-create__input" placeholder="Minimum 6 characters">
+                    <input 
+                        type="password" 
+                        name="password" 
+                        class="user-create__input @error('password') user-create__input--error @enderror" 
+                        placeholder="Minimum 6 characters"
+                    />
+                    @error('password')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
                 <label class="user-create__label">
                     <div class="user-create__desc">Confirm password</div>
-                    <input type="password" name="confirm" class="user-create__input" placeholder="Re-enter the password">
+                    <input type="password" name="confirm" class="user-create__input @error('confirm') user-create__input--error @enderror" placeholder="Re-enter the password">
+                    @error('confirm')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
                 <label class="user-create__label">
                     <div class="user-create__desc">Role</div>
-                    <select name="role" id="pet-select">
+                    <select name="role" id="pet-select" class="user-create__role">
                         <option value="">Select User Role</option>
                         <option value="1">User</option>
                         <option value="2">Reseller</option>
                         <option value="3">Admin</option>
                         <option value="4">Owner</option>
                     </select>
+                    @error('role')
+                        <div class="user-create__error">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </label>
             </div>
             <button class="user-create__button">
